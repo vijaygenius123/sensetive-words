a782dc20a5906e821181409e7e5ba662589d950a
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2021-11-16 10:48:22 +0000

    Update to use @babel/preset-env instead of babel-preset-latest

--- src/index.test.js
+++ src/index.test.js
@@ -1,9 +1,8 @@
-import {sensitiveWords} from '.'
-
-test('it should replace blacklisted words with astrik', () => {
+const {sensitiveWords} = require('./')
+test('it should replace blacklisted words with asterisk', () => {
     expect(sensitiveWords('This is a test', ['a'])).toBe('This is *** test')
 })
 
-test('it should replace multiple occurences of blacklisted words with astrik', () => {
+test('it should replace multiple occurrences of blacklisted words with asterisk', () => {
     expect(sensitiveWords('This is a test. This is a test', ['a'])).toBe('This is *** test. This is *** test')
 })
